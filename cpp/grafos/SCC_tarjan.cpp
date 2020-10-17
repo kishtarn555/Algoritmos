93d9853830697f8be55ba83f0509462cda08a707
kishtarn555 <hec_montemoon@hotmail.com>
2020-10-17 17:26:21 -0500

    Update SCC_tarjan.cpp

--- cpp/grafos/SCC_tarjan.cpp
+++ cpp/grafos/SCC_tarjan.cpp
@@ -1,5 +1,5 @@
 
-void tarjan(int cur, int prev, vector<vector<int> > &_scc,vector<int> * _adj, stack<int> & _st) {
+void tarjan(int cur, int prev, vector<vector<int> > &_scc,vector<int> * _adj, stack<int> & _st, int *lo, int *hi, int &dt) {
     lo[cur]=hi[cur]=dt++;
     _st.push(cur);
     vis[cur]=true;
@@ -9,19 +9,19 @@ void tarjan(int cur, int prev, vector<vector<int> > &_scc,vector<int> * _adj, st
             if (inStack[ni])
                 hi[cur]=min(hi[cur], lo[ni]);
         } else {
-            tarjan(ni, cur,_scc,_adj);
+            tarjan(ni, cur,_scc,_adj,st,lo,hi,st);
             hi[cur]=min(hi[cur], hi[ni]);
         }
     }
     if (lo[cur]==hi[cur]) {
     vector<int> next_cc;
-        while (st.empty()==false && st.top()!=cur) {
-            next_cc.push_back(st.top());
+        while (st.empty()==false && _st.top()!=cur) {
+            next_cc.push_back(_st.top());
             inStack[st.top()]=false;
-            st.pop();
+            _st.pop();
         }
         inStack[cur]=false;
-        st.pop();
+        _st.pop();
         next_cc.push_back(cur);
         _scc.push_back(next_cc);
     }
